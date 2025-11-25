7e79293e621740a1a75939299461e63f44c5a464
bkucse <phuc.buicogang@hcmut.edu.vn>
2025-11-25 15:13:06 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# start.sh - Script để chạy CAS trên Railway
-
-# 1. Cài đặt quyền thực thi nếu cần
-chmod +x ./gradlew
-
-# 2. Chạy ứng dụng Spring Boot qua Gradle
-./gradlew bootRun
+java -jar build/libs/cas.war
